45303f1e26d1c01e321a39c0c6c0870744e65da2
Aseda Gyeke Aboagye <aseda.aboagye@gmail.com>
2014-07-12 10:37:59 -0700

    login: Added single-page navigation.

--- js/default.js
+++ js/default.js
@@ -3,10 +3,13 @@
 (function () {
     "use strict";
 
-    var app = WinJS.Application;
     var activation = Windows.ApplicationModel.Activation;
+    var app = WinJS.Application;
+    var nav = WinJS.Navigation;
+    var sched = WinJS.Utilities.Scheduler;
+    var ui = WinJS.UI;
 
-    app.onactivated = function (args) {
+    app.addEventListener("activated", function (args) {
         if (args.detail.kind === activation.ActivationKind.launch) {
             if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
                 // TODO: This application has been newly launched. Initialize
@@ -18,80 +21,50 @@
                 var field = document.getElementById("token");
                 field.innerText = Windows.Storage.ApplicationData.current.roamingSettings.values["userToken"];
             }
-            args.setPromise(WinJS.UI.processAll());
-            var loginButtonRun = document.getElementById("loginButton");
-            var loggedIn = Windows.Storage.ApplicationData.current.roamingSettings.values["hasToken"];
-            if (null != loginButtonRun)
-                loginButtonRun.addEventListener("click", loginHandler, false);
-            if (loggedIn != true) {
-                loginHandler();
-            }
-            WinJS.Navigation.navigate("/landing.html", {});
+
+            nav.history = app.sessionState.history || {};
+            nav.history.current.initialPlaceholder = true;
+
+            // Optimize the load of the application and while the splash screen is shown, execute high priority scheduled work.
+            ui.disableAnimations();
+            var p = ui.processAll().then(function () {
+                return nav.navigate(nav.location || Application.navigator.home, nav.state);
+            }).then(function () {
+                return sched.requestDrain(sched.Priority.aboveNormal + 1);
+            }).then(function () {
+                ui.enableAnimations();
+            });
+
+            args.setPromise(p);
         }
-    };
+    });
 
     app.oncheckpoint = function (args) {
         // TODO: This application is about to be suspended. Save any state
-        // that needs to persist across suspensions here. You might use the
-        // WinJS.Application.sessionState object, which is automatically
-        // saved and restored across suspension. If you need to complete an
-        // asynchronous operation before your application is suspended, call
-        // args.setPromise().
+        // that needs to persist across suspensions here. If you need to 
+        // complete an asynchronous operation before your application is 
+        // suspended, call args.setPromise().
+        app.sessionState.history = nav.history;
     };
 
-    function loginHandler() {
-        // Launch the request in the iFrame
-        var pushBulletOauthUrl = "https://www.pushbullet.com/authorize";
-        var clientID = "T4bWrUyIoEnoozVHdXZ3bGjH2SZdKRtS";
-        var redirect_uri = "http://127.0.0.1/success.html";
-        var request_url = Windows.Foundation.Uri(pushBulletOauthUrl +
-            "?client_id=" + clientID +
-            "&redirect_uri=" + redirect_uri +
-            "&response_type=token&scope=everything");
-        var callbackUri = Windows.Foundation.Uri(redirect_uri);
+    //var app = WinJS.Application;
+    //var activation = Windows.ApplicationModel.Activation;
 
-        // WebAuthenticationBroker here
-        Windows.Security.Authentication.Web.WebAuthenticationBroker.authenticateAsync(Windows.Security.Authentication.Web.WebAuthenticationOptions.none, request_url, callbackUri).
-            done(
-            function (result) {
-                // Check the response status here
-                switch (result.responseStatus) {
-                    case Windows.Security.Authentication.Web.WebAuthenticationStatus.success:
-                        var fragment = Windows.Foundation.Uri(result.responseData).fragment;
-                        if (fragment.indexOf("#access_token=") != -1) {
-                            var token = fragment.substring(
-                                new String("#access_token=").length);
-
-                            /* Let's store the token */
-                            var appData = Windows.Storage.ApplicationData.current;
-                            var roamingSettings = appData.roamingSettings;
-                            roamingSettings.values["userToken"] = token;
-                            roamingSettings.values["hasToken"] = true;
-                        }
-                        // API calls here.
-                        WinJS.Navigation.navigate("/landing.html");
-                        break;
-
-                    case Windows.Security.Authentication.Web.WebAuthenticationStatus.userCancel:
-                        Log(window.toStaticHTML(result.responseData));
-                        Display("User cancelled the authentication to Facebook");
-                        break;
-
-                    case Windows.Security.Authentication.Web.WebAuthenticationStatus.errorHttp:
-                        Log(window.toStaticHTML(result.responseData));
-                        Display("An error occurred while communicating with Pushbullet");
-                        break;
-                }
-            },
-            function (exception) {
-                Log();
-            },
-            function progress() {
-
-            }
-        );
-        WinJS.Navigation.navigate("/landing.html", {});
-    }
+    //app.onactivated = function (args) {
+    //    if (args.detail.kind === activation.ActivationKind.launch) {
+    //        if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+    //            // TODO: This application has been newly launched. Initialize
+    //            // your application here.
+    //            var appSettings = Windows.Storage.ApplicationData.current.roamingSettings;
+    //        } else {
+    //            // TODO: This application has been reactivated from suspension.
+    //            // Restore application state here.
+    //            var field = document.getElementById("token");
+    //            field.innerText = Windows.Storage.ApplicationData.current.roamingSettings.values["userToken"];
+    //        }
+    //        args.setPromise(WinJS.UI.processAll());
+    //    }
+    //};
 
     app.start();
 })();
---
+++ pages/login/login.js
@@ -0,0 +1,84 @@
+﻿// For an introduction to the Page Control template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232511
+(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/pages/login/login.html", {
+        // This function is called whenever a user navigates to this page. It
+        // populates the page elements with the app's data.
+        ready: function (element, options) {
+            // TODO: Initialize the page here.
+            var loginButtonRun = document.getElementById("loginButton");
+            var loggedIn = Windows.Storage.ApplicationData.current.roamingSettings.values["hasToken"];
+            if (null != loginButtonRun)
+                loginButtonRun.addEventListener("click", loginHandler, false);
+            if (loggedIn != true) {
+                loginHandler();
+            }
+        },
+
+        unload: function () {
+            // TODO: Respond to navigations away from this page.
+        },
+
+        updateLayout: function (element) {
+            /// <param name="element" domElement="true" />
+
+            // TODO: Respond to changes in layout.
+        }
+    });
+
+    function loginHandler() {
+        // Launch the request in the iFrame
+        var pushBulletOauthUrl = "https://www.pushbullet.com/authorize";
+        var clientID = "T4bWrUyIoEnoozVHdXZ3bGjH2SZdKRtS";
+        var redirect_uri = "http://127.0.0.1/success.html";
+        var request_url = Windows.Foundation.Uri(pushBulletOauthUrl +
+            "?client_id=" + clientID +
+            "&redirect_uri=" + redirect_uri +
+            "&response_type=token&scope=everything");
+        var callbackUri = Windows.Foundation.Uri(redirect_uri);
+
+        // WebAuthenticationBroker here
+        Windows.Security.Authentication.Web.WebAuthenticationBroker.authenticateAsync(Windows.Security.Authentication.Web.WebAuthenticationOptions.none, request_url, callbackUri).
+            done(
+            function (result) {
+                // Check the response status here
+                switch (result.responseStatus) {
+                    case Windows.Security.Authentication.Web.WebAuthenticationStatus.success:
+                        var fragment = Windows.Foundation.Uri(result.responseData).fragment;
+                        if (fragment.indexOf("#access_token=") != -1) {
+                            var token = fragment.substring(
+                                new String("#access_token=").length);
+
+                            /* Let's store the token */
+                            var appData = Windows.Storage.ApplicationData.current;
+                            var roamingSettings = appData.roamingSettings;
+                            roamingSettings.values["userToken"] = token;
+                            roamingSettings.values["hasToken"] = true;
+                        }
+                        // API calls here.
+                        //navigate to landing page
+                        WinJS.Navigation.navigate("/pages/landing/landing.html");
+                        break;
+
+                    case Windows.Security.Authentication.Web.WebAuthenticationStatus.userCancel:
+                        Log(window.toStaticHTML(result.responseData));
+                        Display("User cancelled the authentication");
+                        break;
+
+                    case Windows.Security.Authentication.Web.WebAuthenticationStatus.errorHttp:
+                        Log(window.toStaticHTML(result.responseData));
+                        Display("An error occurred while communicating with Pushbullet");
+                        break;
+                }
+            },
+            function (exception) {
+                Log();
+            },
+            function progress() {
+
+            }
+        );
+    }
+})();
