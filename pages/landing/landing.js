86b3fd10f2f251eac2d6f03e85219fa8b394a7cf
Aseda Gyeke Aboagye <aseda.aboagye@gmail.com>
2014-07-13 20:15:59 -0700

    landing: Successfully retrieved list of devices!
    I'm now able to parse and retrieve the nicknames. The next
    step will be to generate icons for each of the devices.

--- pages/landing/landing.js
+++ pages/landing/landing.js
@@ -3,11 +3,26 @@
 (function () {
     "use strict";
 
-    WinJS.UI.Pages.define("/landing.html", {
+    WinJS.UI.Pages.define("/pages/landing/landing.html", {
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
         ready: function (element, options) {
             // TODO: Initialize the page here.
+            // TODO: Get list of devices
+            var token = Windows.Storage.ApplicationData.current.roamingSettings.values["userToken"];
+            WinJS.xhr({ user: token, password: "", url: "https://api.pushbullet.com/v2/devices", responseType: "json", type: "get" }).then(
+                function (response) {
+                    var json = JSON.parse(response.responseText);
+                    var list = new WinJS.Binding.List(json.devices);
+                    var text = WinJS.Utilities.query("#test")[0];
+                    text.innerHTML = list.getItem(0).data.nickname;
+                },
+                function (error) {},
+                function (progress) {});
+
+            var Oldlist = document.getElementById("devices-list").innerHTML;
+            var newList = "<li>Nexus 5</li>";
+            Oldlist = Oldlist + newList;
         },
 
         unload: function () {
@@ -20,4 +35,18 @@
             // TODO: Respond to changes in layout.
         }
     });
+
+    function getDevices() {
+        var token = Windows.Storage.ApplicationData.current.roamingSettings.values["token"];
+        var deviceRequestURI = new Uri("https://" + token + "api.pushbullet.com/v2/devices");
+        var httpClient = new Windows.Web.Http.HttpClient();
+
+        httpClient.getStringAsync(deviceRequestURI).done(function (response) {
+
+        }, onError);
+
+        function onError(reason) {
+            // details in reason.Message and ex.HResult
+        }
+    }
 })();
